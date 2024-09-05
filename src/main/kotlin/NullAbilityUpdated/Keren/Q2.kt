e065874ea83f6ad6f37dec0654f75f6981d03621
Ron Azar <ronazar1999@gmail.com>
2024-09-05 11:15:55 +0300

    Uploading Q2

---
+++ src/main/kotlin/NullAbilityUpdated/Keren/Q2.kt
@@ -0,0 +1,14 @@
+package NullAbilityUpdated.Keren
+/**
+ * Modify the main function to calculate and print the sum of all non-null integers in the list numbers.
+ * Use safe calls and/or the Elvis operator where appropriate.
+ **/
+fun main() {
+    val numbers: List<Int?> = listOf(1, null, 3, null, 5)
+    // Task: Calculate the sum of all non-null numbers
+    println("The sum of all non-null numbers is: ${calculateListSum(numbers)}")
+}
+
+fun calculateListSum(numbers: List<Int?> ): Int{
+    return numbers.sumOf { it ?: 0 }
+}
