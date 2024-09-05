198f2ab212e3a0fe1bbf7d1211c033614891361c
Ron Azar <ronazar1999@gmail.com>
2024-09-05 11:32:35 +0300

    Uploading Q3

---
+++ src/main/kotlin/NullAbilityUpdated/Keren/Q3.kt
@@ -0,0 +1,14 @@
+package NullAbilityUpdated.Keren
+/**
+ * Write an extension function nullSafeToUpper() for String? that converts the string
+ * to uppercase if it is not null, or returns "NO TEXT PROVIDED" if it is null.
+ * Apply this function in the main function to handle the variable text.
+ *
+ **/
+fun main() {
+    val text: String? = null
+    // Task: Create and use an extension function to print text in uppercase if it's not null, or "NO TEXT PROVIDED" if it is null.
+    println(text.printTextInUppercase())
+}
+
+fun String?.printTextInUppercase(): String  = this?.uppercase() ?: "NO TEXT PROVIDED"
