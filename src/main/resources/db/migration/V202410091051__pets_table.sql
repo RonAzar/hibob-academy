51edf297439c74651e34b48d66143a1a9f792a2d
Ron Azar <ronazar1999@gmail.com>
2024-09-10 11:01:00 +0300

    May's Review- DEFAULT date value

--- src/main/resources/db/migration/V202410091051__pets_table.sql
+++ src/main/resources/db/migration/V202410091051__pets_table.sql
@@ -3,8 +3,8 @@ create table pets
     id serial primary key ,
     name varchar(100) not null ,
     type varchar(100) not null ,
-    company_id int,
-    date_of_arrival date not null
+    company_id int not null,
+    date_of_arrival date default CURRENT_DATE
 );
 
 CREATE INDEX idx_pets_company_id ON pets(company_id);
