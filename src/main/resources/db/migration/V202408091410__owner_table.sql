934fdc7003915b4db7868a4bf76832e4a77d66ab
Ron Azar <ronazar1999@gmail.com>
2024-09-10 11:04:40 +0300

    May's Review- should be one index on both columns, and the name should include the table name

--- src/main/resources/db/migration/V202408091410__owner_table.sql
+++ src/main/resources/db/migration/V202408091410__owner_table.sql
@@ -5,5 +5,4 @@ create table owner(
     employee_id varchar(100) not null
 );
 
-CREATE INDEX idx_owner_owner_id ON owner(owner_id);
-CREATE INDEX idx_employee_id ON owner(employee_id);
+CREATE INDEX idx_owner_id_employee_id ON owner(id, employee_id);
