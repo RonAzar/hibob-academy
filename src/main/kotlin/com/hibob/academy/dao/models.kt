4d0de72c4581a9a115c29b61042862e17462330f
Ron Azar <ronazar1999@gmail.com>
2024-09-10 18:33:31 +0300

    Create a function that retrieve all the pets by a given type (represented by enum) and return their name, dateOfArrival and company Id

--- src/main/kotlin/com/hibob/academy/dao/models.kt
+++ src/main/kotlin/com/hibob/academy/dao/models.kt
@@ -1,5 +1,16 @@
 package com.hibob.academy.dao
 
-data class PetData(val petId: Long, val petName: String, val petType: String)
+import java.time.LocalDate
+
+//return their name, dateOfArrival and company Id
+data class PetData(val petName: String, val dateOfArrival: LocalDate, val companyId: Long)
+
+//pets by a given type (represented by enum)
+enum class TYPE{
+    DOG,
+    CAT,
+    HAMSTER,
+    RABBIT
+}
 
 data class Example(val id: Long, val companyId: Long, val data: String)
