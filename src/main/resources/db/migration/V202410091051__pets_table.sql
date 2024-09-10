da4fb7df28ec294f39feed88e882889c89466aae
Ron Azar <ronazar1999@gmail.com>
2024-09-10 11:11:35 +0300

    May's Review- Thank's for the review!

--- src/main/resources/db/migration/V202410091051__pets_table.sql
+++ src/main/resources/db/migration/V202410091051__pets_table.sql
@@ -12,10 +12,10 @@ CREATE INDEX idx_pets_company_id ON pets(company_id);
 -- --Insert data to your pet table for some pets.
 --
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
--- VALUES ('Buddy', 'Dog', "Shi-Tzu", '2024-09-06');
+-- VALUES ('Buddy', 'Dog', 1, '2024-09-06');
 --
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
--- VALUES ('Waffle', 'Dog', "Terrier", '2024-09-06');
+-- VALUES ('Waffle', 'Dog', 1, '2024-09-06');
 --
 --
 -- --Retrieve the pets by type (dog, cat..)
