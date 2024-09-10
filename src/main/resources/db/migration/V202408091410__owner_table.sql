ad5e85de8a64a9cc528255531a884ca0f5710e42
Ron Azar <ronazar1999@gmail.com>
2024-09-10 11:03:16 +0300

    May's Review- employee_id is string in bob also should be not null

--- src/main/resources/db/migration/V202408091410__owner_table.sql
+++ src/main/resources/db/migration/V202408091410__owner_table.sql
@@ -2,7 +2,7 @@ create table owner(
     id serial primary key ,
     name varchar(100) not null ,
     company_id int not null,
-    employee_id int
+    employee_id varchar(100) not null
 );
 
 CREATE INDEX idx_owner_owner_id ON owner(owner_id);
