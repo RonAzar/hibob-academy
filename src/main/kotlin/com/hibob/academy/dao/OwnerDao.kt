20c9ff5fb881748cd53996dbbc02e926ae9b25e4
Ron Azar <ronazar1999@gmail.com>
2024-09-11 08:03:14 +0300

    Create a function that retrieve all the owners from the owner table and return their name, employee Id and company Id.
    +The unit-tests doesn't work yet

--- src/main/kotlin/com/hibob/academy/dao/OwnerDao.kt
+++ src/main/kotlin/com/hibob/academy/dao/OwnerDao.kt
@@ -1,2 +1,23 @@
 package com.hibob.academy.dao
 
+import org.jooq.DSLContext
+import org.jooq.RecordMapper
+import org.jooq.Record
+
+class OwnerDao(private val sql: DSLContext) {
+    private val owner = OwnerTable.instance
+    private val ownerDataMapper = RecordMapper<Record,OwnerData>{ record ->
+            OwnerData(
+                record[owner.ownerName],
+                record[owner.employeeId],
+                record[owner.companyId]
+            )
+    }
+
+    //Create a function that retrieve all the owners from the owner table and return their name, employee Id and company Id
+    fun getAllOwners(): List<OwnerData>{
+        return sql.select(owner.ownerName,owner.employeeId,owner.companyId)
+            .from(owner)
+            .fetch(ownerDataMapper)
+    }
+}
--- src/main/kotlin/com/hibob/academy/dao/models.kt
+++ src/main/kotlin/com/hibob/academy/dao/models.kt
@@ -5,6 +5,9 @@ import java.time.LocalDate
 //return their name, dateOfArrival and company Id
 data class PetData(val petName: String, val dateOfArrival: LocalDate, val companyId: Long)
 
+//return their name, employee Id and company Id.
+data class OwnerData(val ownerName: String, val employeeId: Long, val companyId: Long)
+
 //pets by a given type (represented by enum)
 enum class TYPE{
     DOG,
