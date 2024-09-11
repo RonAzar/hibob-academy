28c5b5aa2950b311536881cf26f226900eacf4d9
Ron Azar <ronazar1999@gmail.com>
2024-09-11 11:49:51 +0300

    unique index

--- src/main/resources/db/migration/V202408091410__owner_table.sql
+++ src/main/resources/db/migration/V202408091410__owner_table.sql
@@ -5,4 +5,4 @@ create table owner(
     employee_id varchar(100) not null
 );
 
-CREATE INDEX idx_owner_company_id_employee_id ON owner(company_id, employee_id);
+CREATE unique index  idx_owner_company_id_employee_id ON owner(company_id, employee_id);
