a20441aed2c27c2e8b3cb77715a312164224c05e
Ron Azar <ronazar1999@gmail.com>
2024-09-14 20:45:27 +0300

    add some comments

--- src/main/kotlin/com/hibob/academy/resource/OwnerResource.kt
+++ src/main/kotlin/com/hibob/academy/resource/OwnerResource.kt
@@ -5,7 +5,7 @@ import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 import org.springframework.stereotype.Controller
 
-@Controller
-@Path("/api/ron/azar/owner/")
-@Produces(MediaType.APPLICATION_JSON)
+@Controller  // Marks the class as a controller, meaning it's responsible for handling incoming HTTP requests
+@Path("/api/ron/azar/owner/")  // Defines the base URL path that this controller will handle
+@Produces(MediaType.APPLICATION_JSON)  // Specifies that the responses produced by this controller will be in JSON format
 class OwnerResource
