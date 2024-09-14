362ce752ccca48e257611d1589b03f95864c7fb6
Ron Azar <ronazar1999@gmail.com>
2024-09-14 20:43:07 +0300

    Adding 2 new Controllers

---
+++ src/main/kotlin/com/hibob/academy/resource/PetsResource.kt
@@ -0,0 +1,11 @@
+package com.hibob.academy.resource
+
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/ron/azar/owner/")
+@Produces(MediaType.APPLICATION_JSON)
+class PetsResource
