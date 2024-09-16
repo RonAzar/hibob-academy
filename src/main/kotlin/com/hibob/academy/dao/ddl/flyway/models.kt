809e33af757473e32bce18758a232542b980faf1
Ron Azar <ronazar1999@gmail.com>
2024-09-16 14:42:54 +0300

    Add the tables in models

--- src/main/kotlin/com/hibob/academy/dao/ddl/flyway/models.kt
+++ src/main/kotlin/com/hibob/academy/dao/ddl/flyway/models.kt
@@ -1,2 +1,9 @@
 package com.hibob.academy.dao.ddl.flyway
 
+import java.time.LocalDate
+
+data class VaccineData(val vaccineId: Long, val vaccineName: String)
+data class Vaccine(val vaccineName: String)
+
+data class VaccineToPetData(val vaccineToPetId: Long, val petId: Long, val vaccinationDate: LocalDate)
+data class VaccineToPet(val petId: Long,  val vaccinationDate: LocalDate)
