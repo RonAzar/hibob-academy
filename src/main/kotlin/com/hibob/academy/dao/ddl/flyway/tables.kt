f5fe684d0bf846da9ee18bf684a10de705e832ae
Ron Azar <ronazar1999@gmail.com>
2024-09-16 14:37:53 +0300

    Add the tables using Jooq table

--- src/main/kotlin/com/hibob/academy/dao/ddl/flyway/tables.kt
+++ src/main/kotlin/com/hibob/academy/dao/ddl/flyway/tables.kt
@@ -1,2 +1,23 @@
 package com.hibob.academy.dao.ddl.flyway
 
+import com.hibob.academy.utils.JooqTable
+
+class VaccineTable(tableName: String = "vaccine"): JooqTable(tableName){
+    val id = createBigIntField("id")
+    val name = createBigIntField("name") // Nullable Long for ownerId
+
+    companion object{
+        val  instance = VaccineTable()
+    }
+}
+
+
+class VaccineToPetTable(tableName: String = "vaccineToPetTable"): JooqTable(tableName){
+    val id = createBigIntField("id")
+    val petId = createVarcharField("pet_id")
+    val vaccinationDate = createBigIntField("vaccination_date")
+
+    companion object{
+        val instance = VaccineToPetTable()
+    }
+}
