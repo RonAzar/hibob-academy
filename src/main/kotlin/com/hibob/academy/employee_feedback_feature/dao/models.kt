113a004ec93411bfa8fd8810d74a10baab0a9aff
Ron Azar <ronazar1999@gmail.com>
2024-09-23 16:49:24 +0300

    Submit new feedback - anonymous submission adding validation that it is actually anonymous after Sofi's review

--- src/main/kotlin/com/hibob/academy/employee_feedback_feature/dao/models.kt
+++ src/main/kotlin/com/hibob/academy/employee_feedback_feature/dao/models.kt
@@ -3,4 +3,4 @@ package com.hibob.academy.employee_feedback_feature.dao
 import java.time.LocalDateTime
 
 data class FeedbackSubmission(val employeeId: Long, val companyId: Long, val feedbackText: String, val isAnonymous: Boolean, val department: String)
-data class FeedbackData(val feedbackId: Long, val employeeId: Long, val companyId: Long, val feedbackText: String, val isAnonymous: Boolean, val department: String, val createdAt: LocalDateTime, val status: Boolean)
+data class FeedbackData(val feedbackId: Long, val employeeId: Long?, val companyId: Long, val feedbackText: String, val isAnonymous: Boolean, val department: String, val createdAt: LocalDateTime, val status: Boolean)
