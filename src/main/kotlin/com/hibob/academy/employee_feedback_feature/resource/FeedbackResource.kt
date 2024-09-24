7bac7d36914bf32bfca2571f4ca8c508a366eaf9
Ron Azar <ronazar1999@gmail.com>
2024-09-24 15:38:21 +0300

    Adding submission POST method

---
+++ src/main/kotlin/com/hibob/academy/employee_feedback_feature/resource/FeedbackResource.kt
@@ -0,0 +1,23 @@
+package com.hibob.academy.employee_feedback_feature.resource
+
+import com.hibob.academy.employee_feedback_feature.dao.FeedbackRequest
+import com.hibob.academy.employee_feedback_feature.service.FeedbackService
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.core.Context
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/feedback")
+class FeedbackResource(private val feedbackService: FeedbackService) {
+    @Path("/submit")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun submitFeedback(@Context requestContext: ContainerRequestContext, newFeedback: FeedbackRequest): Response {
+        return feedbackService.submitFeedback(requestContext, newFeedback)
+    }
+}
