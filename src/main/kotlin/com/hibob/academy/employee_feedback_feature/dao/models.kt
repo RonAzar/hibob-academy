24165d0891992f5d776cccc3123ffde3d26da999
Ron Azar <ronazar1999@gmail.com>
2024-09-24 13:50:21 +0300

    add my FeedbackRequest data class --> use in FeedbackService while submitting feedback

--- src/main/kotlin/com/hibob/academy/employee_feedback_feature/dao/models.kt
+++ src/main/kotlin/com/hibob/academy/employee_feedback_feature/dao/models.kt
@@ -2,15 +2,16 @@ package com.hibob.academy.employee_feedback_feature.dao
 
 import java.time.LocalDateTime
 
+data class FeedbackRequest(val feedbackText: String, val isAnonymous: Boolean, val department: String)
 data class FeedbackSubmission(val employeeId: Long, val companyId: Long, val feedbackText: String, val isAnonymous: Boolean, val department: String)
 data class FeedbackData(val feedbackId: Long, val employeeId: Long?, val companyId: Long, val feedbackText: String, val isAnonymous: Boolean, val department: String, val createdAt: LocalDateTime, val status: Boolean)
 
 data class EmployeeData(val employeeId: Int, val role: EmployeeRole, val companyId: Int)
 data class EmployeeLogin(val firstName: String, val lastName: String, val companyId: Int)
 
-enum class EmployeeRole(val role: String) {
-    ADMIN("admin"),
-    MANAGER("manager"),
-    EMPLOYEE("employee"),
-    HR("hr");
+enum class EmployeeRole{
+    ADMIN,
+    MANAGER,
+    EMPLOYEE,
+    HR;
 }
