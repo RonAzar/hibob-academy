a6639ad97f5112dea8372670b8818b06fc5f241f
Ron Azar <ronazar1999@gmail.com>
2024-09-24 16:02:13 +0300

    Adding getFeedbackHistory method-returning response with list failed

--- src/main/kotlin/com/hibob/academy/employee_feedback_feature/resource/FeedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/employee_feedback_feature/resource/FeedbackResource.kt
@@ -3,6 +3,7 @@ package com.hibob.academy.employee_feedback_feature.resource
 import com.hibob.academy.employee_feedback_feature.dao.FeedbackRequest
 import com.hibob.academy.employee_feedback_feature.service.FeedbackService
 import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.container.ContainerRequestContext
@@ -19,5 +20,11 @@ class FeedbackResource(private val feedbackService: FeedbackService) {
     @Consumes(MediaType.APPLICATION_JSON)
     fun submitFeedback(@Context requestContext: ContainerRequestContext, newFeedback: FeedbackRequest): Response {
         return feedbackService.submitFeedback(requestContext, newFeedback)
+    }
+
+    @Path("/history")
+    @GET
+    fun getFeedbackHistory(@Context requestContext: ContainerRequestContext): Response {
+        return feedbackService.getFeedbackHistory(requestContext)
     }
 }
