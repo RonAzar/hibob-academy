b06ff5c994b14f2cff6fc57c381066af7e21e1a3
Ron Azar <ronazar1999@gmail.com>
2024-09-25 19:31:22 +0300

    Phase 2 - Step 3: Implement FeedbackResponseService.kt with add response functionality Step 3.1: Create test file for FeedbackResponseService to validate response addition logic

---
+++ src/main/kotlin/com/hibob/academy/employee_feedback_feature/service/ResponseService.kt
@@ -0,0 +1,21 @@
+package com.hibob.academy.employee_feedback_feature.service
+
+import com.hibob.academy.employee_feedback_feature.dao.FeedbackDao
+import com.hibob.academy.employee_feedback_feature.dao.FeedbackResponseDao
+import com.hibob.academy.employee_feedback_feature.dao.ResponseSubmission
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class ResponseService @Autowired constructor(
+    private val responseDao: FeedbackResponseDao,
+    private val feedbackDao: FeedbackDao
+) {
+    fun submitResponse(newFeedbackResponse: ResponseSubmission, companyId: Long): Long {
+        val feedbackExists = feedbackDao.getFeedbackByFeedbackId(companyId, newFeedbackResponse.feedbackId)
+        feedbackExists?.let {
+            return responseDao.submitResponse(newFeedbackResponse)
+        }
+        throw IllegalArgumentException("Feedback with ID ${newFeedbackResponse.feedbackId} does not exist.")
+    }
+}
---
+++ src/test/kotlin/com/hibob/academy/employee_feedback_feature/service/ResponseServiceTest.kt
@@ -0,0 +1,60 @@
+package com.hibob.academy.employee_feedback_feature.service
+
+import com.hibob.academy.employee_feedback_feature.dao.FeedbackDao
+import com.hibob.academy.employee_feedback_feature.dao.FeedbackData
+import com.hibob.academy.employee_feedback_feature.dao.FeedbackResponseDao
+import com.hibob.academy.employee_feedback_feature.dao.ResponseSubmission
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.time.LocalDateTime
+
+class ResponseServiceTest {
+    private val companyId = -3L
+    private val feedbackId = -3L
+    private val responseText = "OMG this is the best response ever!"
+    private val responderId = -2L
+    private val responseId = -4L
+
+    private val feedbackDao = mock<FeedbackDao>()
+    private val responseDao = mock<FeedbackResponseDao>()
+    private val responseService = ResponseService(responseDao, feedbackDao)
+
+    @Test
+    fun `submitResponse should submit response and return its ID`() {
+        val testFeedbackData = FeedbackData(
+            feedbackId = feedbackId,
+            employeeId = responderId,
+            companyId = companyId,
+            feedbackText = "What a feedback!",
+            isAnonymous = false,
+            department = "Sales",
+            createdAt = LocalDateTime.now(),
+            status = true
+        )
+
+        whenever(feedbackDao.getFeedbackByFeedbackId(companyId, feedbackId)).thenReturn(testFeedbackData)
+        whenever(responseDao.submitResponse(any())).thenReturn(responseId)
+
+        val responseSubmission = ResponseSubmission(responseText, responderId, feedbackId)
+        val result = responseService.submitResponse(responseSubmission, companyId)
+
+        assertEquals(responseId, result)
+    }
+
+    @Test
+    fun `submitResponse should throw an exception if feedback does not exist`() {
+        whenever(feedbackDao.getFeedbackByFeedbackId(companyId, feedbackId)).thenReturn(null)
+
+        val responseSubmission = ResponseSubmission(responseText, responderId, feedbackId)
+
+        val exception = assertThrows<IllegalArgumentException> {
+            responseService.submitResponse(responseSubmission, companyId)
+        }
+
+        assertEquals("Feedback with ID $feedbackId does not exist.", exception.message)
+    }
+}
