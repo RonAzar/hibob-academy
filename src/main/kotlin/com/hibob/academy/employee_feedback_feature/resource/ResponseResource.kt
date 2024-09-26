29235a12da29e4421a6b9f7b1738fe5fce261d8f
Ron Azar <ronazar1999@gmail.com>
2024-09-26 09:42:17 +0300

    Phase 2 - Step 4: Implement HR Response API
    	•Added FeedbackResponseResource.kt to handle feedback response submissions.
    	•Integrated role and permission checks to ensure authorized access for response submission.

---
+++ src/main/kotlin/com/hibob/academy/employee_feedback_feature/resource/ResponseResource.kt
@@ -0,0 +1,37 @@
+package com.hibob.academy.employee_feedback_feature.resource
+
+import com.hibob.academy.employee_feedback_feature.dao.ResponseSubmission
+import com.hibob.academy.employee_feedback_feature.service.ResponseService
+import com.hibob.academy.employee_feedback_feature.validation.RolePermissionValidator.Companion.Permissions
+import com.hibob.academy.employee_feedback_feature.validation.RolePermissionValidator.Companion.extractClaimAsLong
+import com.hibob.academy.employee_feedback_feature.validation.RolePermissionValidator.Companion.extractRole
+import com.hibob.academy.employee_feedback_feature.validation.RolePermissionValidator.Companion.hasPermission
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.core.Context
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/response")
+class ResponseResource(private val responseService: ResponseService) {
+    @Path("/submit")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun submitResponse(
+        @Context requestContext: ContainerRequestContext,
+        newFeedbackResponse: ResponseSubmission
+    ): Response {
+        val companyId = extractClaimAsLong(requestContext, "companyId")!!
+        val role = extractRole(requestContext)!!
+
+        return if (hasPermission(role, Permissions.RESPONSE_TO_FEEDBACK)) {
+            Response.ok("Response submitted successfully response id: ${responseService.submitResponse(newFeedbackResponse, companyId)}").build()
+        } else {
+            Response.status(Response.Status.UNAUTHORIZED).entity("UNAUTHORIZED: You do not have permission to response feedbacks").build()
+        }
+    }
+}
